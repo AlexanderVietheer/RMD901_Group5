1d53e721ece79df191c0bede1565542ffa50f46c
AlexanderVietheer <alexander.vietheer@mailbox.org>
2022-09-12 09:54:52 +0200

    reading tab delimited data

--- 2022-09-12_day5.R
+++ 2022-09-12_day5.R
@@ -1,4 +1,11 @@
 library(tidyverse)
 library(here)
 here()
-read_csv(here("Data", "Konsultasjoner.csv"))
+myData <- read_csv(here("Data", "Konsultasjoner.csv"))
+myData  # we see the file is tab separated
+
+# reading datafile with read_delim()
+# because the data is tab delimited
+
+myData <- read_delim(here("Data", "Konsultasjoner.csv"), delim = "\t")
+myData
